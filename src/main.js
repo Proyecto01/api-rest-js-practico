1f99b08b4cf4d0a209ff273ba5cd7fb8292e84b5
gparatcha <gparatcha@gmail.com>
2023-08-25 12:39:22 -0300

    Layout and trending-preview list

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-console.log('Hello');
+async function getTrendingMoviesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key='+ API_KEY);
+    const data = await res.json();
+    
+    const movies = data.results;
+    movies.forEach(movie => {
+        const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList');
+
+        const movieContainer = document.createElement('div');
+        movieContainer.classList.add('movie-container');
+        const movieImg = document.createElement('img');
+        movieImg.classList.add('movie-img');
+        movieImg.setAttribute('alt', movie.title);
+        movieImg.setAttribute('src', 'https://image.tmdb.org/t/p/w300'+ movie.poster_path);
+
+        movieContainer.appendChild(movieImg);
+        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    });
+}
+
+getTrendingMoviesPreview();
